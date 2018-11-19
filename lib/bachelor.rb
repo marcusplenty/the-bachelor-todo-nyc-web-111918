59f7f5355f432dffaba7e9eb863eb6c0ea64f883
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-19 04:45:57 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,61 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |k1,v1|
+    if k1.to_s == season 
+      v1.each do |element|
+        if element["status"] == "Winner"
+          x = element["name"]
+          x1 = x.split(" ")
+          return x1[0]
+        end
+      end
+    end 
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  x = ""
+  data.each do |k1,v1|
+      v1.each do |element|
+        if element["occupation"] == occupation
+           return element["name"]
+        end
+      end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+    data.each do |k1,v1|
+      v1.each do |element|
+        if element["hometown"] == hometown
+          counter+=1
+        end
+      end
+    end
+  return counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |k1,v1|
+      v1.find do |element|
+        if element["hometown"] == hometown
+           return element["occupation"]
+        end
+      end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  sum = 0
+  counter = 0
+  data.each do |k1,v1|
+    if k1.to_s == season 
+      v1.each do |element|
+        counter += 1
+        sum += element["age"].to_f
+      end
+    end 
+  end
+  y = sum/counter
+  return y.round
 end
--- spec/bachelor_spec.rb
+++ spec/bachelor_spec.rb
@@ -31,15 +31,15 @@ describe "bachelor" do
   end
 
   describe "#count_contestants_by_hometown" do
-    it "returns 2 when passed data and the string 'New York, New York'" do
+    it "returns 4 when passed data and the string 'New York, New York'" do
       expect(count_contestants_by_hometown(data, "New York, New York")).to eq(4)
     end
 
-    it "returns 6 when passed data and the string 'Chicago, Illinois'" do
+    it "returns 8 when passed data and the string 'Chicago, Illinois'" do
       expect(count_contestants_by_hometown(data, "Chicago, Illinois")).to eq(8)
     end
 
-    it "returns 6 when passed data and the string 'San Diego, CA'" do
+    it "returns 5 when passed data and the string 'San Diego, CA'" do
       expect(count_contestants_by_hometown(data, "San Diego, California")).to eq(5)
     end
   end
