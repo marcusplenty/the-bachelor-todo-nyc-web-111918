072e589a3af2b230254a7a0f0c24b784ee4b665d
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-19 03:05:17 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,8 +2,8 @@ def get_first_name_of_season_winner(data, season)
   data.each do |k1,v1|
     if k1 == season
       v1.each do |element|
-        if data[k1][element]["status:"] == "Winner"
-          return data[k1][element]["name:"]
+        if data[k1][element]["status"] == "Winner"
+          return data[k1][element]["name"]
         end
           
       end 
