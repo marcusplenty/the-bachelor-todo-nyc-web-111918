ab6a7716ee195898d7c3801eac663784a19fdec6
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-19 03:02:17 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,12 @@
 def get_first_name_of_season_winner(data, season)
-  data.
+  data.each do |k1,v1|
+    if k1 == season
+      v1.each do |element|
+        if data[k1]
+        
+      end 
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
