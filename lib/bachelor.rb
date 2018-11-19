f1a81fd528f4e302a2fc3f09d9e462576390bdbd
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-19 03:42:21 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -22,7 +22,15 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  
+  counter = 0
+  data.each do |k1,v1|
+      v1.each do |element|
+        if element[:hometown] == hometown
+          counter+=1
+        end
+      end
+  end
+  return counter
 end
 
 def get_occupation(data, hometown)
